569a0b468d926875b28f0d2884baa7f0718e6774
Chris Wolfe <me@crlfe.ca>
2020-01-20 13:56:38 -0500

    Fix start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ mkdir dist
 live-server --host=localhost --no-browser dist &
 nodemon -w src -w static -e '*' --exec "
   rsync -a static/ dist
-  echo \"<script>$(cat static/index.js)</script>\" >> dist/index.html
+  echo \"<script>\$(cat static/index.js)</script>\" >> dist/index.html
   elm make src/Main.elm --output dist/index.js
 "
